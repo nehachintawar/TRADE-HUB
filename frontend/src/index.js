045d34d7b6bcaf67fb5bbabbd0c5a232d7cbdcc2
neha chintawar <nehachintawar2002@gmail.com>
2025-07-27 00:33:55 +0530

    components updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from '../src/landing_page/home/HomePage'
+import AboutPage from '../src/landing_page/about/AboutPage';
+import PricingPage from '../src/landing_page/pricing/PricingPage';
+import ProductPage from '../src/landing_page/products/ProductPage';
+import SupportPage from '../src/landing_page/support/SupportPage';
+
+
 
 
 
@@ -10,6 +16,10 @@ root.render(
   <React.StrictMode>
     <div>
       <HomePage />
+      <AboutPage/>
+      <PricingPage/>
+      <ProductPage/>
+      <SupportPage/>
     </div>
   </React.StrictMode>
 );
---
+++ frontend/src/landing_page/support/SupportPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import CreateTicket from './CreateTicket';
+import Hero from './Hero';
+
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+
+
+
+function SupportPage() {
+    return ( 
+        <>
+        <CreateTicket/>
+        <Hero/>
+        <Navbar/>
+        <Footer/>
+        </>
+     );
+}
+
+export default SupportPage;
