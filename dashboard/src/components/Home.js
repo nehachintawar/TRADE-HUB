7629211795c72410c150c62453e39856a62687eb
neha chintawar <nehachintawar2002@gmail.com>
2025-07-30 15:27:33 +0530

    Creating Dashboard Components

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -1,1 +1,15 @@
-Me
+import React from "react";
+
+import Dashboard from "./Dashboard";
+import TopBar from "./TopBar";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
